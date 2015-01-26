725ec81388310023773d4ca57d3f9163b41d2772
Andres Galaviz <andres.galaviz@gmail.com>
2015-01-26 12:59:08 -0600

    Utilizar 2 blogs

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,18 @@ var router = express.Router();
 // For Ghost
 var httpProxy = require('http-proxy');
 var blogProxy = httpProxy.createProxyServer({});
-var ghost     = require('ghost');
-var path      = require('path');
+// var ghost     = require('ghost');
+// var path      = require('path');
 
 // Route /blog* to Ghost
-router.use("/blog/", function(req, res){ 
+router.use("/ghost/", function(req, res){ 
     blogProxy.web(req, res, { target: 'http://localhost:2368' });
 });
 ghost({config: path.join(__dirname, '../ghost/config.js')}).then(function (ghostServer) {
      ghostServer.start();
+});
+router.use("/blog/", function(req, res){ 
+    blogProxy.web(req, res, { target: 'blogrqpidea.azurewebsites.net' });
 });
 /* GET home page. */
 router.get('/', function(req, res, next) {
